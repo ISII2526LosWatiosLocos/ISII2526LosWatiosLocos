6d7ef92b08c3249463baef19764b1ae7d5c089a5
Mar <Mar@MAR>
2025-10-02 00:02:14 +0200

    Crear clase reparaciónItem

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -5,5 +5,5 @@ using AppForSEII2526.API.Models;
 namespace AppForSEII2526.API.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options) {
-    public DbSet<ReparaciónItem> ReparaciónItems { get; set; }
+    public DbSet<ReparaciónItem> ReparaciónItems { get; set; } 
 }
--- src/AppForSEII2526.API/Models/ReparaciónItem.cs
+++ src/AppForSEII2526.API/Models/ReparaciónItem.cs
@@ -1,17 +1,20 @@
 ﻿namespace AppForSEII2526.API.Models
 {
     public class ReparaciónItem
-    {
+    { 
         [Key]
         public int IdReparación { get; set; }
 
         [Required]
         public int cantidad { get; set; }
 
+        [Required]
         public String Descripción { get; set; }
 
+        [Required]
         public int IdHerramienta { get; set; }
 
+        [Required]
         public float Precio { get; set; }
     }
 }
