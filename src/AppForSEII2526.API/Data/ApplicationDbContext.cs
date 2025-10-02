01cc918e88db6b018a66c15dafd7b39a65a8a42b
DimasUCLM <DimasAntonio.Torralba@alu.uclm.es>
2025-10-02 10:44:36 +0200

    modificado ApplicationDbContext.cs

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -5,4 +5,5 @@ using AppForSEII2526.API.Models;
 namespace AppForSEII2526.API.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options) {
+    public DbSet<Compra> Compras { get; set; }
 }
