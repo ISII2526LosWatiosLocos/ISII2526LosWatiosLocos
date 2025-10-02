bc5552915b5c58cf33883e77e3361f307bce8743
DimasUCLM <dimasantonio.torralba@alu.uclm.es>
2025-10-02 13:02:41 +0200

    Creada la clase CompraItem (#15)
    * Creada la clase CompraItem
    
    * modificado ApplicationDbContext.cs

---
+++ src/AppForSEII2526.API/Models/CompraItem.cs
@@ -0,0 +1,17 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class CompraItem
+    {
+        [Key]
+        public int IdCompra { get; set; }
+
+        [Required]
+        public int IdHerramienta { get; set; }
+
+        public int Cantidad { get; set; }
+
+        public int Descripción { get; set; }
+
+        public float Precio { get; set; }
+    }
+}
