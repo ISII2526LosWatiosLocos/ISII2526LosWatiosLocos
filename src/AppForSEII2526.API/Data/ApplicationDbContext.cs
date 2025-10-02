8b5fddd103558837a48aa525b42c2a619bade1ee
Mar <Mar@MAR>
2025-10-02 18:12:32 +0200

    Creación clase Reparación

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -5,4 +5,9 @@ using AppForSEII2526.API.Models;
 namespace AppForSEII2526.API.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options) {
+    public DbSet<Reparación> reparaciones { get; set; }
+    public DbSet<ReparaciónItem> ReparaciónItems { get; set; }
+    public DbSet<Herramienta> Herramientas { get; set; }
+    public DbSet<Fabricante> Fabricantes { get; set; }
+
 }
