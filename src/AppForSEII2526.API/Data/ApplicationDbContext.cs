18332edd0f08bbb1eee303375997679684b1af42
YoelCasa <yoel.casamayor@alu.uclm.es>
2025-10-02 07:11:12 +0200

    Creada la clase herramienta (#9)
    Merge de la rama de "Creada la clase herramienta" a la rama development

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -5,5 +5,8 @@ using AppForSEII2526.API.Models;
 namespace AppForSEII2526.API.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options) {
+
+    public DbSet<Herramienta> Herramientas { get; set; }
     public DbSet<Fabricante> Fabricantes { get; set; }
+
 }
