694ea24b8d8a864d5c0c9c2a6a08c6dd9b62ab6c
Jesus <Jesus.Arribas2@alu.uclm.es>
2025-10-02 19:33:54 +0200

    Creación de clase AlquilarItem

---
+++ src/AppForSEII2526.API/Models/AlquilarItem.cs
@@ -0,0 +1,15 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class AlquilarItem
+    {
+        [Key]
+        public int IdAlquiler { get; set; }
+
+        [Required]
+        public int IdHerramienta { get; set; }
+        [Required]
+        public float Precio { get; set; }
+        [Required]
+        public int Cantidad { get; set; }
+    }
+}
