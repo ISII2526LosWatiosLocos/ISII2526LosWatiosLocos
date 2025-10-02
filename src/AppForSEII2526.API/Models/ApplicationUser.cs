aec2859c897a296f5dd2ee1fbf9c69fa7ba83667
YOEL CASAMAYOR SAHUQUILLO <Yoel.Casamayor@alu.uclm.es>
2025-10-02 07:40:33 +0200

    Modificada la clase ApplicationUser con los atributos adicionales requeridos

--- src/AppForSEII2526.API/Models/ApplicationUser.cs
+++ src/AppForSEII2526.API/Models/ApplicationUser.cs
@@ -4,4 +4,12 @@ namespace AppForSEII2526.API.Models;
 
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser {
+    [Required, StringLength(50, ErrorMessage = "Numero de caracteres excedido. No se puede más de 50 caracteres")]
+    public string Nombre { get; set; }
+
+    [Required, StringLength(100, ErrorMessage = "Numero de caracteres excedido. No se puede más de 100 caracteres")]
+    public string Apellidos { get; set; }
+
+    [StringLength(20, ErrorMessage = "Numero de caracteres excedido. No se puede más de 20 caracteres")]
+    public string Correo { get; set; }
 }
