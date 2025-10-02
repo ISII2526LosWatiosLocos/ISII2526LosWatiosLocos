9e92314f9dcbc1ff6071eed16071b4f882f57a40
YoelCasa <yoel.casamayor@alu.uclm.es>
2025-10-02 07:13:36 +0200

    Creada la clase Oferta (#13)
    Unida la rama "crear la clase oferta" a la rama development

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -5,7 +5,7 @@ using AppForSEII2526.API.Models;
 namespace AppForSEII2526.API.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options) {
-
+    public DbSet<Oferta> Ofertas { get; set; }
     public DbSet<OfertaItem> OfertaItems { get; set; }
     public DbSet<Herramienta> Herramientas { get; set; }
     public DbSet<Fabricante> Fabricantes { get; set; }
