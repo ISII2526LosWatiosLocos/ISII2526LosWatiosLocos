71714b37d7ddddf918e44cd72bcfb685138a9047
Mar <Mar@MAR>
2025-10-03 12:48:36 +0200

    Reparación línea duplicada.

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -5,9 +5,7 @@ using AppForSEII2526.API.Models;
 namespace AppForSEII2526.API.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options) {
-    public DbSet<ReparaciónItem> ReparaciónItems { get; set; } 
-
-    public DbSet<Reparación> reparaciones { get; set; }
+    public DbSet<Reparación> Reparaciones { get; set; }
     public DbSet<Compra> Compras { get; set; }
     public DbSet<CompraItem> CompraItems { get; set; }
     public DbSet<Oferta> Ofertas { get; set; }
