517b81fae35e37550a645b6ff086c6167cc0c074
maraqqq22 <maria.plaza8@alu.uclm.es>
2025-10-03 11:27:20 +0200

    Creación clase Reparación (#35)
    * Creación clase Reparación
    
    * Creación clase RepararItem
    
    ---------
    
    Co-authored-by: Mar <Mar@MAR>
    Co-authored-by: DimasUCLM <dimasantonio.torralba@alu.uclm.es>

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -7,6 +7,10 @@ namespace AppForSEII2526.API.Data;
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options) {
     public DbSet<ReparaciónItem> ReparaciónItems { get; set; } 
 
+    public DbSet<Reparación> reparaciones { get; set; }
+    public DbSet<ReparaciónItem> ReparaciónItems { get; set; }
+    
+
     public DbSet<Compra> Compras { get; set; }
     public DbSet<CompraItem> CompraItems { get; set; }
     public DbSet<Oferta> Ofertas { get; set; }
@@ -28,4 +32,5 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
     {
         base.OnModelCreating(builder);
     }
+
 }
--- src/AppForSEII2526.API/Models/Reparación.cs
+++ src/AppForSEII2526.API/Models/Reparación.cs
@@ -1,8 +1,38 @@
-﻿namespace AppForSEII2526.API.Models
+using System.ComponentModel.DataAnnotations;
+namespace AppForSEII2526.API.Models
 {
     public class Reparación
     {
+        [Key]
+        public int Id { get; set; }
 
-    
+
+            [Required]
+            [DataType(System.ComponentModel.DataAnnotations.DataType.Text), Display(Name = "Apellido Cliente")]
+            public string ApellidoCliente { get; set; }
+
+            [Required]
+            [DataType(System.ComponentModel.DataAnnotations.DataType.Date), Display(Name = "Fecha Entrega")]
+            [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+            public DateTime FechaEntrega { get; set; }
+
+            [Required]
+            [DataType(System.ComponentModel.DataAnnotations.DataType.Date), Display(Name = "Fecha Recogida")]
+            [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+            public DateTime FechaRecogida { get; set; }
+
+            [Required]
+            [DataType(System.ComponentModel.DataAnnotations.DataType.Text), Display(Name = "Nombre Cliente")]
+            public string NombreCliente { get; set; }
+
+            [Required]
+            [DataType(System.ComponentModel.DataAnnotations.DataType.PhoneNumber), Display(Name = "Número Teléfono")]
+            public string NumTelefono { get; set; }
+
+            [Required]
+            [DataType(System.ComponentModel.DataAnnotations.DataType.Currency), Display(Name = "Precio Total")]
+            public float PrecioTotal { get; set; }
+
+        
     }
 }
