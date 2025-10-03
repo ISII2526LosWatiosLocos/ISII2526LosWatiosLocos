f8038237e546cf8e7ccfb25f7cf3f3f335f5add0
Jesus <Jesus.Arribas2@alu.uclm.es>
2025-10-03 21:11:35 +0200

    Creada clase AlquilarItem junto a sus relaciones y atributos formateados IdAlquiler, IdHerramienta, Precio y Cantidad

--- src/AppForSEII2526.API/Models/AlquilarItem.cs
+++ src/AppForSEII2526.API/Models/AlquilarItem.cs
@@ -10,6 +10,10 @@
         [Required]
         public float Precio { get; set; }
         [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "La cantidad debe ser al menos 1.")]
         public int Cantidad { get; set; }
+        // Relaciones
+        public Alquiler Alquiler { get; set; }
+        public Herramienta Herramienta { get; set; }
     }
 }
