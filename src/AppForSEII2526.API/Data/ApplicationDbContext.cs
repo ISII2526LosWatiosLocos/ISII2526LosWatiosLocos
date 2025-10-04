4b1b282d2fb862c19bbe8cabce8e00e457015073
YoelCasa <yoel.casamayor@alu.uclm.es>
2025-10-04 12:58:39 +0200

    Creada la enum sin fallos (#65)

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -20,9 +20,6 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
     public DbSet<TarjetaCredito> TarjetasCredito { get; set; }
     public DbSet<Paypal> Paypals { get; set; }
 
-    public DbSet<DirigidaOferta> DirigidasOfertas { get; set; }
-    public DbSet<Cliente> Clientes { get; set; }
-    public DbSet<Socio> Socios { get; set; }
 
 
     protected override void OnModelCreating(ModelBuilder builder)
