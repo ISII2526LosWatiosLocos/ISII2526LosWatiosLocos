856b6d2b23bae20745cea7549c2ee3e674b5a399
DimasUCLM <DimasAntonio.Torralba@alu.uclm.es>
2025-10-04 20:39:43 +0200

    retocadas clases Compra y CompraItem

--- src/AppForSEII2526.API/Models/Compra.cs
+++ src/AppForSEII2526.API/Models/Compra.cs
@@ -12,16 +12,15 @@
         [Required]
         public string DirecciónEnvío { get; set; }
         [Required]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateOnly FechaCompra { get; set; }
         [Required]
         public float PrecioTotal { get; set; }
-        [Required]
         public List<CompraItem> CompraItems { get; set; }
-        [Required]
         public MetodosPago MetodoPago { get; set; }
 
-        public int Teléfono { get; set; }
+        public int ?Teléfono { get; set; }
 
-        public string CorreoElectrónico { get; set; }
+        public string ?CorreoElectrónico { get; set; }
     }
 }
