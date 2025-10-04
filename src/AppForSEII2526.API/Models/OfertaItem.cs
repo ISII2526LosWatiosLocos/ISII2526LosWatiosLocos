fa03dfebcb401b39047f148682e9a8bf2145fbe3
YOEL CASAMAYOR SAHUQUILLO <Yoel.Casamayor@alu.uclm.es>
2025-10-04 11:41:28 +0200

    Modificados ciertos atributos en funcion de su obligatoriedad/opcionalidad en funcion a la descripcion del caso de uso

--- src/AppForSEII2526.API/Models/OfertaItem.cs
+++ src/AppForSEII2526.API/Models/OfertaItem.cs
@@ -8,6 +8,8 @@
         [Required]
         public int IdHerramienta { get; set; }
 
+        [Required(ErrorMessage = "El porcentaje es obligatorio.")]
+        [Range(0, 100, ErrorMessage = "El porcentaje debe estar entre 0 y 100.")]
         public int Porcentaje { get; set; }
 
         [Required]
