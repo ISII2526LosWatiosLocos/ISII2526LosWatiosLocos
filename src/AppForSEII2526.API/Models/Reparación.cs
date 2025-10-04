0e99834b29bb1bc7f7dc54cb9313f8d63fc900ff
Mar <Mar@MAR>
2025-10-04 13:03:46 +0200

    Opcionalidad atributo de Reparación  corregida.

--- src/AppForSEII2526.API/Models/Reparación.cs
+++ src/AppForSEII2526.API/Models/Reparación.cs
@@ -25,14 +25,19 @@ namespace AppForSEII2526.API.Models
             [DataType(System.ComponentModel.DataAnnotations.DataType.Text), Display(Name = "Nombre Cliente")]
             public string NombreCliente { get; set; }
 
-            [Required]
+            
             [DataType(System.ComponentModel.DataAnnotations.DataType.PhoneNumber), Display(Name = "Número Teléfono")]
-            public string NumTelefono { get; set; }
+            public string? NumTelefono { get; set; }
+
+
 
             [Required]
             [DataType(System.ComponentModel.DataAnnotations.DataType.Currency), Display(Name = "Precio Total")]
             public float PrecioTotal { get; set; }
 
-        
+            [Required]
+            public MetodosPago MétodoPago { get; set; }
+
+
     }
 }
