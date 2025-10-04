db0bdfc1b88f0c422ec1bdc5037511f201aef11f
Yisus <jesus.arribas2@alu.uclm.es>
2025-10-04 11:44:37 +0200

    34 crear clase alquileritem (#60)
    * Creación de clase AlquilarItem
    
    * Creada clase AlquilarItem junto a sus relaciones y atributos formateados IdAlquiler, IdHerramienta, Precio y Cantidad

---
+++ src/AppForSEII2526.API/Models/AlquilarItem.cs
@@ -0,0 +1,19 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class AlquilarItem
+    {
+        [Key]
+        public int IdAlquiler { get; set; }
+
+        [Required]
+        public int IdHerramienta { get; set; }
+        [Required]
+        public float Precio { get; set; }
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "La cantidad debe ser al menos 1.")]
+        public int Cantidad { get; set; }
+        // Relaciones
+        public Alquiler Alquiler { get; set; }
+        public Herramienta Herramienta { get; set; }
+    }
+}
