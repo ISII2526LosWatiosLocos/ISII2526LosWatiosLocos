dcb8de1145b1e7b0c812b2becc31f8af68a7a8ce
maraqqq22 <maria.plaza8@alu.uclm.es>
2025-10-04 17:16:22 +0200

    Opcionalidad atributo de Reparación  corregida. (#71)
    Co-authored-by: Mar <Mar@MAR>

--- src/AppForSEII2526.API/Models/Reparación.cs
+++ src/AppForSEII2526.API/Models/Reparación.cs
@@ -25,14 +25,18 @@ namespace AppForSEII2526.API.Models
             [DataType(System.ComponentModel.DataAnnotations.DataType.Text), Display(Name = "Nombre Cliente")]
             public string NombreCliente { get; set; }
 
-            [Required]
+            
             [DataType(System.ComponentModel.DataAnnotations.DataType.PhoneNumber), Display(Name = "Número Teléfono")]
-            public string NumTelefono { get; set; }
+            public string? NumTelefono { get; set; }
+
+
 
             [Required]
             [DataType(System.ComponentModel.DataAnnotations.DataType.Currency), Display(Name = "Precio Total")]
             public float PrecioTotal { get; set; }
 
+            [Required]
+            public MetodosPago MétodoPago { get; set; }
         //Relaciones 
         public List<ReparaciónItem> ReparaciónItems { get; set; }
 
