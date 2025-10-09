aad41bf3a86120623e163b4a2ba38d748bdf9376
DimasUCLM <dimasantonio.torralba@alu.uclm.es>
2025-10-09 07:03:32 +0200

    Modificadas las clases Compra y CompraItem. No he modificado AplicationUser porque los cambios a realizar ya estaban hechos.

--- src/AppForSEII2526.API/Models/Compra.cs
+++ src/AppForSEII2526.API/Models/Compra.cs
@@ -1,14 +1,11 @@
 ﻿namespace AppForSEII2526.API.Models
 {
+    [PrimaryKey(nameof(CompraId))]
     public class Compra
     {
-        [Key]
-        public int Id { get; set; }
 
-        [Required]
-        public string NombreCliente { get; set; }
-        [Required]
-        public string ApellidoCliente { get; set; }
+        public int CompraId { get; set; }
+
         [Required]
         public string DirecciónEnvío { get; set; }
         [Required]
@@ -16,11 +13,8 @@
         public DateOnly FechaCompra { get; set; }
         [Required]
         public float PrecioTotal { get; set; }
+
         public List<CompraItem> CompraItems { get; set; }
         public MetodosPago MetodoPago { get; set; }
-
-        public int ?Teléfono { get; set; }
-
-        public string ?CorreoElectrónico { get; set; }
     }
 }
--- src/AppForSEII2526.API/Models/CompraItem.cs
+++ src/AppForSEII2526.API/Models/CompraItem.cs
@@ -1,18 +1,19 @@
 ﻿namespace AppForSEII2526.API.Models
 {
+    [PrimaryKey(nameof(CompraItemId), nameof(HerramientaId))]
     public class CompraItem
     {
-        [Key]
-        public int IdCompra { get; set; }
 
-        [Required]
-        public int IdHerramienta { get; set; }
+        public int CompraItemId { get; set; }
+        public int HerramientaId { get; set; }
+
         [Required]
         public int Cantidad { get; set; }
         [Required]
         public int Descripción { get; set; }
         [Required]
         public float Precio { get; set; }
+
         public Herramienta Herramienta { get; set; }
         public Compra Compra { get; set; }
     }
