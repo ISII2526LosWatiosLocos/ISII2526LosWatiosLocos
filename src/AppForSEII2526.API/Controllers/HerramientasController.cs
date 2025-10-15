f0a7c45c43aa551b691231e65071385930be6f87
YoelCasa <yoel.casamayor@alu.uclm.es>
2025-10-15 12:46:33 +0200

    Creado el controlador (#124)

---
+++ src/AppForSEII2526.API/Controllers/HerramientasController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HerramientasController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<HerramientasController> _logger;
+
+        public HerramientasController(ApplicationDbContext context, ILogger<HerramientasController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+
+    }
+}
