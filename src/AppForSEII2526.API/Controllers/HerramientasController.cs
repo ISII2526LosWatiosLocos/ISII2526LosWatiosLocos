71c14fdc4d876949844326c1aea65542b7698e3b
DimasUCLM <dimasantonio.torralba@alu.uclm.es>
2025-10-15 10:50:10 +0200

    creado controlador

---
+++ src/AppForSEII2526.API/Controllers/HerramientasController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HerramientasController : ControllerBase
+    {
+
+        //used to enable your controller to access to the database
+        private readonly ApplicationDbContext _context;
+
+        //used to log any information when your system is running
+        private readonly ILogger<HerramientasController> _logger;
+
+        public HerramientasController(ApplicationDbContext context, ILogger<HerramientasController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+    }
+}
