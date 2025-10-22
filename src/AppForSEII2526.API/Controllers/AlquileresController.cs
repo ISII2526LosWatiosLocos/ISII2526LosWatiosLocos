68fa4c4fca7e702680c89f218bfefd824237f45e
Jesus <Jesus.Arribas2@alu.uclm.es>
2025-10-22 13:01:02 +0200

    Creación de la clase controladora de los alquileres

---
+++ src/AppForSEII2526.API/Controllers/AlquileresController.cs
@@ -0,0 +1,26 @@
+﻿using AppForSEII2526.API.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlquileresController : ControllerBase
+    {
+
+        //used to enable your controller to access to the database
+        private readonly ApplicationDbContext _context;
+
+        //used to log any information when your system is running
+        private readonly ILogger<HerramientasController> _logger;
+
+        public AlquileresController(ApplicationDbContext context, ILogger<HerramientasController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+    }
+
+}
