e0b346ab9ed7cc9334b5a241d6a7f63b1e5a1a05
Mar <Mar@MAR>
2025-10-23 11:18:48 +0200

    Implementación del get Detalle de ReparacionesController

--- src/AppForSEII2526.API/Controllers/ReparacionesController.cs
+++ src/AppForSEII2526.API/Controllers/ReparacionesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AppForSEII2526.API.DTOs;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AppForSEII2526.API.Controllers
@@ -18,6 +19,37 @@ namespace AppForSEII2526.API.Controllers
             _logger = logger;
         }
 
-        
+        [HttpGet]
+        [Route("Detalle-Reparaciones")]
+        [ProducesResponseType(typeof(IList<ReparacionesDTO>), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetDetalleHerramientasParaReparación()
+        {
+            var reparaciones = await _context.Reparaciones
+                .Include(r => r.MétodoPago)
+                .Include(r => r.ReparaciónItems)
+                    .ThenInclude(ri => ri.Herramienta)
+                        .ThenInclude(h => h.Fabricante)
+                .ToListAsync();
+
+
+            var reparacionesDTO = reparaciones.Select(r => new ReparacionesDTO(
+                r.Usuario.Nombre,
+                r.Usuario.Apellidos,
+                r.FechaEntrega,
+                r.FechaRecogida,
+                r.PrecioTotal,
+                r.ReparaciónItems.Select(ri => ri.Herramienta.Nombre).FirstOrDefault()!,
+                r.ReparaciónItems.Select(ri => ri.Herramienta.Precio).FirstOrDefault(),
+                r.ReparaciónItems.Select(ri => ri.Descripción).FirstOrDefault(),
+                r.ReparaciónItems.Select(ri => ri.cantidad).FirstOrDefault()
+
+
+
+            )).ToList();
+
+            return Ok(reparacionesDTO);
+        }
+
+
     }
 }
