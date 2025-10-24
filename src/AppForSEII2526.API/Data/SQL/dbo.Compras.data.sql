14b59ed8c3626ce06fcdd271642864048501baad
YOEL CASAMAYOR SAHUQUILLO <Yoel.Casamayor@alu.uclm.es>
2025-10-24 10:10:42 +0200

    Todo solucionado

--- src/AppForSEII2526.API/Data/SQL/dbo.Compras.data.sql
+++ src/AppForSEII2526.API/Data/SQL/dbo.Compras.data.sql
@@ -1,3 +1,3 @@
 ﻿SET IDENTITY_INSERT [dbo].[Compras] ON
-INSERT INTO [dbo].[Compras] ([Id], [DirecciónEnvío], [FechaCompra], [PrecioTotal], [MétodoPagoId], [UsuarioId]) VALUES (3, N'C/Fermin 33', N'2030-10-10', 120, 1, N'1')
+INSERT INTO [dbo].[Compras] ([Id], [DireccionEnvio], [FechaCompra], [PrecioTotal], [MetodoPagoId], [UsuarioId]) VALUES (3, N'C/Fermin 33', N'2030-10-10', 120, 1, N'1')
 SET IDENTITY_INSERT [dbo].[Compras] OFF
