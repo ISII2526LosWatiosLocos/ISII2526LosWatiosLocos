1d6dc22af1132fb5abfd2aaf221b2410b84fe9d9
Jesus <Jesus.Arribas2@alu.uclm.es>
2025-10-26 09:39:31 +0100

    Para cada herramienta seleccionada se pedirá de forma obligatoria la cantidad a alquilar.

---
+++ src/AppForSEII2526.API/DTOs/CrearAlquilerItemDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace AppForSEII2526.API.DTOs
+{
+    public class CrearAlquilerItemDTO
+    {
+        public int HerramientaId { get; set; }
+        public int HerramientaCantidad { get; set; }
+    }
+}
