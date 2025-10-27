82a6af5f963f3f18f85396114fe9183edd3e993a
Jesus <Jesus.Arribas2@alu.uclm.es>
2025-10-27 22:36:16 +0100

    Creación de DTOs para el post

---
+++ src/AppForSEII2526.API/DTOs/CrearAlquilerDTO.cs
@@ -0,0 +1,42 @@
+﻿
+namespace AppForSEII2526.API.DTOs
+{
+    public class CrearAlquilerDTO
+    {
+        public CrearAlquilerDTO(string nombre, string apellidos, int metodoPagoId, string direccion, string telefono, string? correo, List<CrearAlquilerItemDTO> items)
+        {
+            Nombre = nombre;
+            Apellidos = apellidos;
+            MetodoPagoId = metodoPagoId;
+            Direccion = direccion;
+            this.telefono = telefono;
+            this.correo = correo;
+            Items = items;
+        }
+
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public int MetodoPagoId { get; set; }
+        public string Direccion {  get; set; }
+        public string telefono { get; set; }
+        public string? correo { get; set; }
+        public List<CrearAlquilerItemDTO> Items { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is CrearAlquilerDTO dTO &&
+                   Nombre == dTO.Nombre &&
+                   Apellidos == dTO.Apellidos &&
+                   MetodoPagoId == dTO.MetodoPagoId &&
+                   Direccion == dTO.Direccion &&
+                   telefono == dTO.telefono &&
+                   correo == dTO.correo &&
+                   EqualityComparer<List<CrearAlquilerItemDTO>>.Default.Equals(Items, dTO.Items);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Nombre, Apellidos, MetodoPagoId, Direccion, telefono, correo, Items);
+        }
+    }
+}
--- src/AppForSEII2526.API/DTOs/CrearAlquilerItemDTO.cs
+++ src/AppForSEII2526.API/DTOs/CrearAlquilerItemDTO.cs
@@ -1,8 +1,28 @@
-﻿namespace AppForSEII2526.API.DTOs
+﻿
+namespace AppForSEII2526.API.DTOs
 {
     public class CrearAlquilerItemDTO
     {
+        public CrearAlquilerItemDTO(int herramientaId, int herramientaCantidad)
+        {
+            HerramientaId = herramientaId;
+            HerramientaCantidad = herramientaCantidad;
+        }
+
         public int HerramientaId { get; set; }
         public int HerramientaCantidad { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is CrearAlquilerItemDTO dTO &&
+                   HerramientaId == dTO.HerramientaId &&
+                   HerramientaCantidad == dTO.HerramientaCantidad;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(HerramientaId, HerramientaCantidad);
+        }
     }
+
 }
