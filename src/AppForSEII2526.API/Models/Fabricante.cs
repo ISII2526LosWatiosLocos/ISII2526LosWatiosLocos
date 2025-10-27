0f58182defc744e0f5b97a76abb059f1b11b463d
YOEL CASAMAYOR SAHUQUILLO <Yoel.Casamayor@alu.uclm.es>
2025-10-27 13:22:21 +0100

    Creados constructores y carpeta para pruebas

--- src/AppForSEII2526.API/Models/Fabricante.cs
+++ src/AppForSEII2526.API/Models/Fabricante.cs
@@ -10,5 +10,16 @@
 
         public List<Herramienta> Herramientas { get; set; }
 
+        public Fabricante(int id, string nombre, List<Herramienta> herramientas)
+        {
+            Id = id;
+            Nombre = nombre;
+            Herramientas = herramientas;
+        }
+
+        public Fabricante()
+        {
+            
+        }
     }
 }
