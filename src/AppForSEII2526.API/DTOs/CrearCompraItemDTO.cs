5a0445b816d75bee35a02cbd739435600159af35
DimasUCLM <dimasantonio.torralba@alu.uclm.es>
2025-10-28 10:25:35 +0100

    Arreglado para el correcto uso de la clave compuesta (#216)
    Arregladet

--- src/AppForSEII2526.API/DTOs/CrearCompraItemDTO.cs
+++ src/AppForSEII2526.API/DTOs/CrearCompraItemDTO.cs
@@ -3,6 +3,7 @@
     public class CrearCompraItemDTO
     {
         // El ID de la Herramienta a la que se aplica la compra
+        public int CompraId { get; set; }
         public int HerramientaId { get; set; }
         public int Cantidad { get; set; }
         public string Descripcion { get; set; }
