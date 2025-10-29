505f8ddd5eb1171270866d1de125852ff273fa29
Jesús Arribas Gómez <Jesus.Arribas2@alu.uclm.es>
2025-10-29 08:36:20 +0100

    Mejorado CrearAlquilerDTO

--- src/AppForSEII2526.API/DTOs/CrearAlquilerDTO.cs
+++ src/AppForSEII2526.API/DTOs/CrearAlquilerDTO.cs
@@ -1,14 +1,11 @@
 ﻿
+
 namespace AppForSEII2526.API.DTOs
 {
     public class CrearAlquilerDTO
     {
-        public CrearAlquilerDTO(float precioTotal, DateTime fechaAlquiler, DateTime fechaInicio, DateTime fechaFinal, string nombre, string apellidos, int metodoPagoId, string direccion, string telefono, string? correo, List<CrearAlquilerItemDTO> items)
+        public CrearAlquilerDTO(string nombre, string apellidos, int metodoPagoId, string direccion, string telefono, string? correo, List<CrearAlquilerItemDTO> items)
         {
-            PrecioTotal = precioTotal;
-            FechaAlquiler = fechaAlquiler;
-            FechaInicio = fechaInicio;
-            FechaFinal = fechaFinal;
             Nombre = nombre;
             Apellidos = apellidos;
             MetodoPagoId = metodoPagoId;
@@ -18,10 +15,6 @@ namespace AppForSEII2526.API.DTOs
             Items = items;
         }
 
-        public float PrecioTotal { get; set; }
-        public DateTime FechaAlquiler {  get; set; }
-        public DateTime FechaInicio { get; set; }
-        public DateTime FechaFinal {  get; set; }
         public string Nombre { get; set; }
         public string Apellidos { get; set; }
         public int MetodoPagoId { get; set; }
@@ -33,8 +26,6 @@ namespace AppForSEII2526.API.DTOs
         public override bool Equals(object? obj)
         {
             return obj is CrearAlquilerDTO dTO &&
-                   FechaInicio == dTO.FechaInicio &&
-                   FechaFinal == dTO.FechaFinal &&
                    Nombre == dTO.Nombre &&
                    Apellidos == dTO.Apellidos &&
                    MetodoPagoId == dTO.MetodoPagoId &&
@@ -46,17 +37,7 @@ namespace AppForSEII2526.API.DTOs
 
         public override int GetHashCode()
         {
-            HashCode hash = new HashCode();
-            hash.Add(FechaInicio);
-            hash.Add(FechaFinal);
-            hash.Add(Nombre);
-            hash.Add(Apellidos);
-            hash.Add(MetodoPagoId);
-            hash.Add(Direccion);
-            hash.Add(telefono);
-            hash.Add(correo);
-            hash.Add(Items);
-            return hash.ToHashCode();
+            return HashCode.Combine(Nombre, Apellidos, MetodoPagoId, Direccion, telefono, correo, Items);
         }
     }
 }
