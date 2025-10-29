b92d8ae85bb9f14df3c35600908365181f4b8eb3
Jesús Arribas Gómez <Jesus.Arribas2@alu.uclm.es>
2025-10-29 08:55:10 +0100

    correo pertenece a application users y no a alquiler

--- src/AppForSEII2526.API/Models/Alquiler.cs
+++ src/AppForSEII2526.API/Models/Alquiler.cs
@@ -7,14 +7,13 @@ namespace AppForSEII2526.API.Models
     {
         public Alquiler() { }
 
-        public Alquiler(string direccionEnvio, DateTime fechaAlquiler, DateTime fechaInicio, DateTime fechaFin, float precioTotal, string? correo, List<AlquilarItem> alquilarItems, MetodosPago métodoPago, ApplicationUser usuario)
+        public Alquiler(string direccionEnvio, DateTime fechaAlquiler, DateTime fechaInicio, DateTime fechaFin, float precioTotal, List<AlquilarItem> alquilarItems, MetodosPago métodoPago, ApplicationUser usuario)
         {
             DireccionEnvio = direccionEnvio;
             FechaAlquiler = fechaAlquiler;
             FechaInicio = fechaInicio;
             FechaFin = fechaFin;
             PrecioTotal = precioTotal;
-            Correo = correo;
             AlquilarItems = alquilarItems;
             MétodoPago = métodoPago;
             Usuario = usuario;
@@ -50,11 +49,6 @@ namespace AppForSEII2526.API.Models
         [Required]
         [DataType(System.ComponentModel.DataAnnotations.DataType.Currency), Display(Name = "Precio Total")]
         public float PrecioTotal { get; set; }
-
-        // Campos opcionales
-
-        [DataType(System.ComponentModel.DataAnnotations.DataType.EmailAddress), Display(Name = "Correo Electrónico")]
-        public string ?Correo { get; set; }
 
         // Relaciones
         public List<AlquilarItem> AlquilarItems { get; set; }
