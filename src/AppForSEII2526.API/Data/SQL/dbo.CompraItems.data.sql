da528892c0315d42043b242db8ecd72ba795e0d9
DimasUCLM <dimasantonio.torralba@alu.uclm.es>
2025-10-29 17:47:16 +0100

    eliminados SQLs que dan errores de global, ya cada uno arregla los que le correspondan, y eliminados los que si se han metido a global

--- src/AppForSEII2526.API/Data/SQL/dbo.CompraItems.data.sql
+++
@@ -1,2 +0,0 @@
-﻿INSERT INTO [dbo].[CompraItems] ([CompraId], [HerramientaId], [Cantidad], [Descripcion], [Precio]) VALUES (3, 1, 2, N'Hola', 10)
-INSERT INTO [dbo].[CompraItems] ([CompraId], [HerramientaId], [Cantidad], [Descripcion], [Precio]) VALUES (3, 2, 3, N'Adios', 20)
--- src/AppForSEII2526.API/Data/SQL/dbo.Global.data.sql
+++ src/AppForSEII2526.API/Data/SQL/dbo.Global.data.sql
@@ -22,23 +22,9 @@ SET IDENTITY_INSERT [dbo].[Compras] OFF
 INSERT INTO [dbo].[CompraItems] ([CompraId], [HerramientaId], [Cantidad], [Descripcion], [Precio]) VALUES (3, 1, 2, N'Hola', 10)
 INSERT INTO [dbo].[CompraItems] ([CompraId], [HerramientaId], [Cantidad], [Descripcion], [Precio]) VALUES (3, 2, 3, N'Adios', 20)
 
-SET IDENTITY_INSERT [dbo].[Reparaciones] ON
-INSERT INTO [dbo].[Reparaciones] ([Id], [FechaEntrega], [FechaRecogida], [PrecioTotal], [MétodoPagoId], [UsuarioId]) VALUES (1, N'2028-10-10 00:00:00', N'2027-10-10 00:00:00', 1234, 1, N'1')
-SET IDENTITY_INSERT [dbo].[Reparaciones] OFF
-
-INSERT INTO [dbo].[ReparaciónItems] ([ReparaciónId], [HerramientaId], [cantidad], [Descripción], [Precio]) VALUES (1, 1, 1, N'Holaaaa', 120)
-INSERT INTO [dbo].[ReparaciónItems] ([ReparaciónId], [HerramientaId], [cantidad], [Descripción], [Precio]) VALUES (1, 2, 2, N'fhjskdnfdskl', 1256)
-
 SET IDENTITY_INSERT [dbo].[Ofertas] ON
 INSERT INTO [dbo].[Ofertas] ([Id], [FechaFinal], [FechaInicio], [FechaOferta], [TipoDirigida], [MetodosPagoId]) VALUES (3, N'2026-10-10 00:00:00', N'2027-10-10 00:00:00', N'2026-11-10 00:00:00', 1, 1)
 SET IDENTITY_INSERT [dbo].[Ofertas] OFF
 
 INSERT INTO [dbo].[OfertaItems] ([OfertaId], [HerramientaId], [Porcentaje], [PrecioFinal]) VALUES (3, 1, 10, 18)
 INSERT INTO [dbo].[OfertaItems] ([OfertaId], [HerramientaId], [Porcentaje], [PrecioFinal]) VALUES (3, 2, 20, 15)
-
-SET IDENTITY_INSERT [dbo].[Alquileres] ON
-INSERT INTO [dbo].[Alquileres] ([Id], [DireccionEnvio], [FechaAlquiler], [FechaInicio], [FechaFin], [PrecioTotal], [Correo], [MétodoPagoId], [UsuarioId]) VALUES (1, N'C/Dimas 33', N'2090-10-10 00:00:00', N'2010-10-10 00:00:00', N'3190-10-10 00:00:00', 1221212, N'dimas@gmail.com', 1, N'1')
-SET IDENTITY_INSERT [dbo].[Alquileres] OFF
-
-INSERT INTO [dbo].[AlquilarItems] ([AlquilerId], [HerramientaId], [Precio], [Cantidad]) VALUES (1, 1, 100, 10)
-INSERT INTO [dbo].[AlquilarItems] ([AlquilerId], [HerramientaId], [Precio], [Cantidad]) VALUES (1, 2, 10, 10)
