7fed48f963128864d51f50e75e834bfb9da6a7e8
YOEL CASAMAYOR SAHUQUILLO <Yoel.Casamayor@alu.uclm.es>
2025-10-31 16:27:46 +0100

    Pruebas del get del detalle de oferta implementadas

--- src/AppForSEII2526.API/Models/Paypal.cs
+++ src/AppForSEII2526.API/Models/Paypal.cs
@@ -2,5 +2,9 @@
 {
     public class Paypal : MetodosPago
     {
+        public override string ToString()
+        {
+            return Nombre; //devuelve "Paypal" en lugar del nombre completo del tipo
+        }
     }
 }
