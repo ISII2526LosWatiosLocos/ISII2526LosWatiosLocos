8285cd580760330560495bd1e70c97f3c6d0e302
YOEL CASAMAYOR SAHUQUILLO <Yoel.Casamayor@alu.uclm.es>
2025-10-31 16:46:23 +0100

    Arreglado el usuario

--- src/AppForSEII2526.API/Models/ApplicationUser.cs
+++ src/AppForSEII2526.API/Models/ApplicationUser.cs
@@ -42,14 +42,13 @@ public class ApplicationUser : IdentityUser {
     // Constructor vacío
     public ApplicationUser() { }
 
-    public ApplicationUser(string id, string nombre, string apellidos, string email, string direccion, string telefono)
+    public ApplicationUser(string id, string nombre, string apellidos, string email, string telefono)
     {
         Id = id;
         Nombre = nombre;
         Apellidos = apellidos;
         Email = email;
         UserName = email;
-        CorreoElectronico = direccion;
         NumeroTelefono = telefono;
     }
 
