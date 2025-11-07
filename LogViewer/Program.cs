c0e67ae2c250e32317dbf24c6bd208f17d00324e
YOEL CASAMAYOR SAHUQUILLO <Yoel.Casamayor@alu.uclm.es>
2025-11-07 09:30:29 +0100

    Primera parte hecha

---
+++ LogViewer/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace LogViewer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // 1. Crea una instancia de tu suscriptor
+            var subscriber = new Subscriber();
+
+            // 2. Llama al método que inicia la escucha
+            subscriber.StartReceiving();
+        }
+    }
+}
