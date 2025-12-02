510ada56a4050da3e51e47a42b1c0128ce0cf460
DimasUCLM <dimasantonio.torralba@alu.uclm.es>
2025-12-02 08:30:45 +0100

    añadidos al CrearCompraDTO atributos opcionales

--- src/AppForSEII2526.API/DTOs/CrearCompraDTO.cs
+++ src/AppForSEII2526.API/DTOs/CrearCompraDTO.cs
@@ -8,6 +8,10 @@
         public List<CompraItemsDTO> Items { get; set; }
         public string DireccionEnvio { get; set; }
 
+        //Opcionales:
+        public string? NumeroTelefono { get; set; }
+        public string? CorreoElectronico { get; set; }
+
         public CrearCompraDTO()
         {
             Items = new List<CompraItemsDTO>();
