0c20ac063bf9e2a2ab4e8d8dea3854984097497c
DimasUCLM <dimasantonio.torralba@alu.uclm.es>
2025-12-03 16:55:50 +0100

    añadido el ID a ComprasParaDetalleDTO

--- src/AppForSEII2526.API/DTOs/ComprasParaDetalleDTO.cs
+++ src/AppForSEII2526.API/DTOs/ComprasParaDetalleDTO.cs
@@ -2,6 +2,7 @@
 {
     public class ComprasParaDetalleDTO
     {
+        public int IdCompra {  get; set; }
         public string Nombre { get; set; }
         public string Apellidos { get; set; }
         public string DireccionEnvio { get; set; }
@@ -9,8 +10,9 @@
         public DateOnly FechaCompra { get; set; }
         public IList<CompraItemsDTO> Items { get; set; } // Lista de Items
 
-        public ComprasParaDetalleDTO(string Nombre, string Apellidos, string DireccionEnvio, float PrecioTotal, DateOnly FechaCompra, IList<CompraItemsDTO> Items)
+        public ComprasParaDetalleDTO(int IdCompra, string Nombre, string Apellidos, string DireccionEnvio, float PrecioTotal, DateOnly FechaCompra, IList<CompraItemsDTO> Items)
         {
+            this.IdCompra = IdCompra;
             this.Nombre = Nombre;
             this.Apellidos = Apellidos;
             this.DireccionEnvio = DireccionEnvio;
@@ -22,6 +24,7 @@
         public override bool Equals(object? obj)
         {
             return obj is ComprasParaDetalleDTO dTO &&
+                   IdCompra == dTO.IdCompra &&
                    Nombre == dTO.Nombre &&
                    Apellidos == dTO.Apellidos &&
                    DireccionEnvio == dTO.DireccionEnvio &&
@@ -34,7 +37,7 @@
 
         public override int GetHashCode()
         {
-            return HashCode.Combine(Nombre, Apellidos, DireccionEnvio, PrecioTotal, FechaCompra, Items);
+            return HashCode.Combine(IdCompra, Nombre, Apellidos, DireccionEnvio, PrecioTotal, FechaCompra, Items);
         }
     }
 }
