0cb2a522ef120b88807d345678e36b7974368878
DimasUCLM <dimasantonio.torralba@alu.uclm.es>
2025-12-04 09:10:41 +0100

    añadidos al CrearCompraDTO atributos opcionales (#352)
    * añadidos al CrearCompraDTO atributos opcionales
    
    * acutalizada validación del usuario en ComprasController
    
    * arreglado los tests para acomodar los cambios
    
    * bugs arreglados ahora si
    
    * por fin joder

--- src/AppForSEII2526.API/Controllers/ComprasController.cs
+++ src/AppForSEII2526.API/Controllers/ComprasController.cs
@@ -160,7 +160,22 @@ namespace AppForSEII2526.API.Controllers
 
             // b. Buscar Usuario
             var Usuario = await _context.Users.FirstOrDefaultAsync(u=>u.Nombre == CrearCompraDTO.Nombre && u.Apellidos == CrearCompraDTO.Apellidos);
-            if (Usuario == null) ModelState.AddModelError(nameof(CrearCompraDTO.Nombre), $"El usuario no existe.");
+            if (Usuario == null)
+            {
+                ModelState.AddModelError(nameof(CrearCompraDTO.Nombre), $"El usuario no existe.");
+            }
+            else //sobreescribo las variables opcionales si son proporcionadas, tal y como me ha dicho Noelia
+            {
+                if ((CrearCompraDTO.NumeroTelefono != null) && (CrearCompraDTO.NumeroTelefono != ""))
+                {
+                    Usuario.NumeroTelefono = CrearCompraDTO.NumeroTelefono;
+                }
+                if ((CrearCompraDTO.CorreoElectronico != null) && (CrearCompraDTO.CorreoElectronico != ""))
+                {
+                    Usuario.CorreoElectronico = CrearCompraDTO.CorreoElectronico;
+                }
+            }
+
 
             // c. Validar items del DTO (que no tengan valores imposibles)
             // Solo comprobación estructural mínima aquí: existencia de la lista y validación básica del IdHerramienta.
--- src/AppForSEII2526.API/DTOs/CrearCompraDTO.cs
+++ src/AppForSEII2526.API/DTOs/CrearCompraDTO.cs
@@ -8,6 +8,10 @@
         public List<CompraItemsDTO> Items { get; set; }
         public string DireccionEnvio { get; set; }
 
+        //Opcionales:
+        public string? CorreoElectronico { get; set; }
+        public string? NumeroTelefono { get; set; }
+
         public CrearCompraDTO()
         {
             Items = new List<CompraItemsDTO>();
