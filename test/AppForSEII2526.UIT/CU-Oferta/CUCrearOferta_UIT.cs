f86280c35d929e4dedea58f090b315b25d2a5d22
YOEL CASAMAYOR SAHUQUILLO <Yoel.Casamayor@alu.uclm.es>
2025-12-05 09:12:30 +0100

    Implementamos la clase para las pruebas UIT

---
+++ test/AppForSEII2526.UIT/CU-Oferta/CUCrearOferta_UIT.cs
@@ -0,0 +1,16 @@
+﻿using AppForMovies.UIT.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.CU_Oferta
+{
+    public class CUCrearOferta_UIT : UC_UIT
+    {
+        public CUCrearOferta_UIT(ITestOutputHelper output) : base(output)
+        {
+        }
+    }
+}
