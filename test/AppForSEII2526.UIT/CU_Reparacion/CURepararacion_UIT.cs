85110f702f64e682417d2f4cbd9f370b16f69f19
Mar <Mar@MAR>
2025-12-10 00:46:00 +0100

    Crear l clase para implementar los casos de la UIT

---
+++ test/AppForSEII2526.UIT/CU_Reparacion/CURepararacion_UIT.cs
@@ -0,0 +1,18 @@
+﻿
+using AppForMovies.UIT.Shared;
+using AppForSEII2526.UIT.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.CU_Reparacion
+{
+    public class CURepararacion_UIT: UC_UIT 
+    {
+        public CURepararacion_UIT(ITestOutputHelper output) : base(output)
+        {
+        }
+    }
+}
