158dc004eab396e59e1a89efd63e588fe0f6d822
Jesuswag <Jesus.Arribas2@alu.uclm.es>
2025-12-13 17:36:23 +0100

    Page object del details

---
+++ test/AppForSEII2526.UIT/CU-Alquiler/DetalleAlquiler_PO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.CU_Alquiler
+{
+    public class DetalleAlquiler_PO: PageObject
+    {
+        public DetalleAlquiler_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
+        {
+        }
+
+        public bool CheckDetallesAlquiler(List<string[]> expectedDetails)
+        {
+            return CheckBodyTable(expectedDetails, By.Id("TableOfAlquileres"));
+        }
+    }
+}
