ebe5526981e4bc5b3e721c8e7c4589476ef5a3d7
Jesuswag <Jesus.Arribas2@alu.uclm.es>
2025-12-13 12:23:40 +0100

    Creamos la clase para implementar los casos UIT del alquiler

---
+++ test/AppForSEII2526.UIT/CU-Alquiler/UC_AlquilarHerramientas_UIT.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.CU_Alquiler
+{
+    public class UC_AlquilarHerramientas_UIT: UC_UIT
+    {
+        public UC_AlquilarHerramientas_UIT(ITestOutputHelper output) : base(output)
+        {
+        }
+    }
+}
