80b91b9c7e32575665bb10bf0afb9393937b8dbc
Jesuswag <Jesus.Arribas2@alu.uclm.es>
2025-12-13 16:28:47 +0100

    Primeras constantes e instanciación del PO del select

--- test/AppForSEII2526.UIT/CU-Alquiler/UC_AlquilarHerramientas_UIT.cs
+++ test/AppForSEII2526.UIT/CU-Alquiler/UC_AlquilarHerramientas_UIT.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AppForMovies.UIT.Shared;
+using AppForSEII2526.UIT.CU_Oferta;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +8,19 @@ using System.Threading.Tasks;
 
 namespace AppForSEII2526.UIT.CU_Alquiler
 {
+    // PARA PROBAR LOS TESTS HAY QUE DAR CLICK DERECHO, VER, ABRIR CON EL NAVEGADOR A LA API Y A LA WEB
+
     public class UC_AlquilarHerramientas_UIT: UC_UIT
     {
+        private const string herramientaNombre1 = "Martillo";
+        private const string herramientaMaterial1 = "Madera";
+
+        private readonly SelectHerramientasParaAlquilar_PO _selectPO;
+
         public UC_AlquilarHerramientas_UIT(ITestOutputHelper output) : base(output)
         {
+            _selectPO = new SelectHerramientasParaAlquilar_PO(_driver, output);
+
         }
     }
 }
