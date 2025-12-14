78cce8bf5b7e91e9d0ff160838bbfa776496e8b5
Yisus <jesus.arribas2@alu.uclm.es>
2025-12-14 23:23:29 +0100

    346 pruebas de la iu del select con selenium cu4 (#419)
    * Creamos la clase para implementar los casos UIT del alquiler
    
    * Creado el Page Object + arreglar ids
    
    * Añadimos los filtros nombre y material
    
    * Check lista herramientas
    
    * Añadir herramientas al carro del alquiler
    
    * continuar alquiler
    
    * Menú ahora tiene el pos del alquiler + ids
    
    * Primeras constantes e instanciación del PO del select
    
    * Creamos la clase para implementar los casos UIT del alquiler
    
    * Creado el Page Object + arreglar ids
    
    * Añadimos los filtros nombre y material
    
    * Check lista herramientas
    
    * Añadir herramientas al carro del alquiler
    
    * continuar alquiler
    
    * Menú ahora tiene el pos del alquiler + ids
    
    * Primeras constantes e instanciación del PO del select
    
    * Page Object del Post
    
    * Page object del details
    
    * Parte del UC_AlquilarHerramientas_UIT.cs + retoques
    
    * Más mejoras del select y el post de Page Object
    
    * metodopago y cantidad arreglado po crear alquiler
    
    * Primer flujo conseguido!

---
+++ test/AppForSEII2526.UIT/CU-Alquiler/CrearAlquiler_PO.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using AppForSEII2526.UIT.Shared;
+using Xunit.Abstractions;
+
+namespace AppForSEII2526.UIT.CU_Alquiler
+{
+    public class CrearAlquiler_PO: PageObject
+    {
+        // IDs extraídos de CrearAlquiler.razor
+        private By _nombreUsuario = By.Id("Name");
+        private By _apellidosUsuario = By.Id("Surname");
+        private By _direccion = By.Id("DeliveryAddress");
+        private By _fechaInicio = By.Id("InitialDate");
+        private By _fechaFin = By.Id("FinalDate");
+        private By _telefono = By.Id("Phone");
+        private By _correo = By.Id("Email");
+        private By _metodoPago = By.Id("PaymentMethod");
+        private By _submitButton = By.Id("Submit");
+        private By _errorsShown = By.Id("ErrorsShown");
+
+        public CrearAlquiler_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
+        {
+        }
+
+        public void RellenarDatosGenerales( DateTime inicio,
+                                            DateTime fin,
+                                            string usuario,
+                                            string apellidos,
+                                            string direccion,
+                                            string telefono,
+                                            string correo,
+                                            string metodoPago)
+        {
+
+            WaitForBeingVisible(_nombreUsuario);
+            _driver.FindElement(_nombreUsuario).Clear();
+            _driver.FindElement(_nombreUsuario).SendKeys(usuario);
+
+            WaitForBeingVisible(_apellidosUsuario);
+            _driver.FindElement(_apellidosUsuario).Clear();
+            _driver.FindElement(_apellidosUsuario).SendKeys(apellidos);
+
+            WaitForBeingVisible(_direccion);
+            _driver.FindElement(_direccion).Clear();
+            _driver.FindElement(_direccion).SendKeys(direccion);
+
+            WaitForBeingVisible(_telefono);
+            _driver.FindElement(_telefono).Clear();
+            _driver.FindElement(_telefono).SendKeys(telefono);
+
+            WaitForBeingVisible(_correo);
+            _driver.FindElement(_correo).Clear();
+            _driver.FindElement(_correo).SendKeys(correo);
+            
+            // Fechas (usando método heredado de PageObject)
+            WaitForBeingVisible(_fechaInicio);
+            InputDateInDatePicker(_fechaInicio, inicio);
+
+            WaitForBeingVisible(_fechaFin);
+            InputDateInDatePicker(_fechaFin, fin);
+
+            var select = new SelectElement(_driver.FindElement(_metodoPago));
+            // select.SelectByValue("1"); // Por value (PayPal)
+            select.SelectByText(metodoPago);
+
+        }
+
+        // acepta int para evitar ambigüedades y envía el número correcto al input
+        public void EstablecerCantidad(int herramientaId, int cantidad)
+        {
+            By inputCantidad = By.Id($"cantidad_{herramientaId}");
+
+            WaitForBeingVisible(inputCantidad);
+            var element = _driver.FindElement(inputCantidad);
+
+            // Borra y escribe el número correcto
+            element.Clear();
+            // En algunos navegadores/input tipo number puede ser necesario enviar como string
+            element.SendKeys(cantidad.ToString());
+        }
+
+        public void PulsarCrearAlquiler()
+        {
+            WaitForBeingClickable(_submitButton);
+            _driver.FindElement(_submitButton).Click();
+        }
+        public void ConfirmarModal()
+        {
+            // Usa el método ya existente en tu PageObject base
+            PressOkModalDialog();
+        }
+        public bool CheckErrorMessage(string message)
+        {
+            try
+            {
+                WaitForBeingVisible(_errorsShown);
+                string actualError = _driver.FindElement(_errorsShown).Text;
+                return actualError.Contains(message);
+            }
+            catch (WebDriverTimeoutException)
+            {
+                return false; // No salió el mensaje de error
+            }
+        }
+    }
+}
---
+++ test/AppForSEII2526.UIT/CU-Alquiler/SelectHerramientasParaAlquilar_PO.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.CU_Alquiler
+{
+    public class SelectHerramientasParaAlquilar_PO : PageObject
+    {
+        By inputNombre = By.Id("inputNombreHerramienta");
+        By inputMaterial = By.Id("inputMaterialHerramienta");
+        By tablaHerramientas = By.Id("TablaDeHerramientas");
+        private By botonContinuar = By.Id("btn_continuar_alquiler");
+        public SelectHerramientasParaAlquilar_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
+        {
+        }
+        public void BuscarHerramientas(string nombre, string material)
+        {
+            WaitForBeingVisible(inputNombre);
+            var nom = _driver.FindElement(inputNombre);
+            nom.Clear();
+            nom.SendKeys(nombre);
+
+            WaitForBeingVisible(inputMaterial);
+            var mat = _driver.FindElement(inputMaterial);
+            mat.Clear();
+            mat.SendKeys(material);
+        }
+
+        public bool CheckListOfHerramientas(List<string[]> expectedHerramientas)
+        {
+            return CheckBodyTable(expectedHerramientas, tablaHerramientas);
+        }
+        public void AñadirHerramientasAlCarroDeAlquiler(string nombreHerramienta)
+        {
+            By btnAddLocator = By.Id($"herramientaParaAlquilar_{nombreHerramienta}");
+            // Esperar y clicar
+            WaitForBeingClickable(btnAddLocator);
+            _driver.FindElement(btnAddLocator).Click();
+            By btnRemoveLocator = By.Id($"quitarHerramienta_{nombreHerramienta}");
+            WaitForBeingVisible(btnRemoveLocator);
+        }
+        public void Continuar()
+        {
+            // Como hemos esperado al carrito arriba, este botón ya debería estar habilitado
+            WaitForBeingClickable(botonContinuar);
+            _driver.FindElement(botonContinuar).Click();
+        }
+    }
+}
