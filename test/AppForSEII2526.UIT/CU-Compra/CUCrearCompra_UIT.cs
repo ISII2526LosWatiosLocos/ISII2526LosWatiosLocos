e5e10cca665a37fba5366b8085061c9846b832e9
DimasUCLM <dimasantonio.torralba@alu.uclm.es>
2025-12-16 15:57:14 +0100

    mas pruebas

--- test/AppForSEII2526.UIT/CU-Compra/CUCrearCompra_UIT.cs
+++ test/AppForSEII2526.UIT/CU-Compra/CUCrearCompra_UIT.cs
@@ -63,7 +63,7 @@ namespace AppForSEII2526.UIT.CU_Compra
         }
 
         // -------------------------------------------------------------------
-        // [Theory]: ESCENARIO 1: DESCRIPCIÓN INVÁLIDA
+        // [Theory]: DESCRIPCIÓN INVÁLIDA
         // -------------------------------------------------------------------
         [Theory]
         [Trait("Category", "UIT")]
@@ -103,7 +103,7 @@ namespace AppForSEII2526.UIT.CU_Compra
         }
 
         // -------------------------------------------------------------------
-        // [Fact]: ESCENARIO 2: USUARIO NO EXISTE
+        // [Fact]: USUARIO NO EXISTE
         // -------------------------------------------------------------------
         [Fact]
         [Trait("Category", "UIT")]
@@ -141,7 +141,7 @@ namespace AppForSEII2526.UIT.CU_Compra
         }
 
         // -------------------------------------------------------------------
-        // [Fact]: ESCENARIO 3: CARRITO VACÍO
+        // [Fact]: CARRITO VACÍO
         // -------------------------------------------------------------------
         [Fact]
         [Trait("Category", "UIT")]
@@ -163,7 +163,7 @@ namespace AppForSEII2526.UIT.CU_Compra
         }
 
         // -------------------------------------------------------------------
-        // [Fact]: ESCENARIO 4: CARRITO VACÍO (despues de añadir y quitar herramientas)
+        // [Fact]: CARRITO VACÍO (despues de añadir y quitar herramientas)
         // -------------------------------------------------------------------
         [Fact]
         [Trait("Category", "UIT")]
@@ -194,6 +194,93 @@ namespace AppForSEII2526.UIT.CU_Compra
             // 3. ASSERT
             bool seguimosEnSeleccion = _driver.Url.Contains("/Compra/SelectHerramientasParaCompra");
             Assert.True(seguimosEnSeleccion, "El sistema permitió continuar con el carrito vacío.");
+        }
+
+        // -------------------------------------------------------------------
+        // [Fact]: Faltan datos obligatorios
+        // -------------------------------------------------------------------
+        [Fact]
+        [Trait("Category", "UIT")]
+        public void UC1_5_DatosObligatoriosFaltantes_Error()
+        {
+            // 1. ARRANGE
+            string nombre = "Yoel";
+            string apellidos = "CS";
+            string direccionEnvio = ""; // falta dirección
+            string pagoValue = "0";
+
+            int herramientaId = 1;
+            string nombreHerramienta = "Martillo";
+            string material = "Madera";
+            string precio = "10";
+
+            // 2. ACT
+            // Navegar
+            _driver.Navigate().GoToUrl(_URI + "Compra/SelectHerramientasParaCompra");
+
+            // Buscar y Seleccionar
+            _selectPO.BuscarHerramientas(material, precio);
+            _selectPO.AñadirHerramientasAlCarroDeCompra(nombreHerramienta);
+            _selectPO.Continuar();
+
+            // Rellenar Formulario
+            _crearPO.RellenarDatosGenerales(nombre, apellidos, direccionEnvio, pagoValue);
+            _crearPO.RellenarDescripcion(herramientaId, "Descripción cualquiera"); // cualquier descripción no nula es válida
+
+            // Confirmar
+            _crearPO.PulsarCrearCompra();
+            _crearPO.ConfirmarModal();
+
+            // 3. ASSERT
+            System.Threading.Thread.Sleep(1000); // DEJAMOS UN POCO DE TIEMPO PARA PROCESAR
+            bool hayError = _crearPO.CheckErrorMessage("La compra debe tener una dirección de envío.") || _crearPO.CheckErrorMessage("Error");
+            Assert.True(hayError, "El sistema no mostró error al ser la dirección inexistente.");
+        }
+
+        // -------------------------------------------------------------------
+        // [Fact]: COMPRAR MULTIPLES ITEMS: (Creación Exitosa)
+        // -------------------------------------------------------------------
+        [Fact]
+        [Trait("Category", "UIT")]
+        public void UC1_6_CompraMultiplesItems_Exito()
+        {
+            // 1. ARRANGE
+            string nombre = "Yoel";
+            string apellidos = "CS";
+            string direccionEnvio = "casa de yoel";
+            string pagoValue = "0";
+
+            string material = "";
+            string precio = "";
+
+            // 2. ACT
+            // Navegar
+            _driver.Navigate().GoToUrl(_URI + "Compra/SelectHerramientasParaCompra");
+
+            // Buscar y Seleccionar
+            _selectPO.BuscarHerramientas(material, precio);
+            _selectPO.AñadirHerramientasAlCarroDeCompra("Martillo");
+            _selectPO.AñadirHerramientasAlCarroDeCompra("Martillo");
+            _selectPO.AñadirHerramientasAlCarroDeCompra("Llave");
+            _selectPO.borrarHerramienta("Llave");
+            _selectPO.AñadirHerramientasAlCarroDeCompra("Llave");
+            _selectPO.AñadirHerramientasAlCarroDeCompra("Llave");
+            _selectPO.Continuar();
+
+            // Rellenar Formulario
+            _crearPO.RellenarDatosGenerales(nombre, apellidos, direccionEnvio, pagoValue);
+            _crearPO.RellenarDescripcion(1, "Descripción martillo cualquiera"); // cualquier descripción no nula es válida
+            _crearPO.RellenarDescripcion(2, "Descripción martillo cualquiera"); // cualquier descripción no nula es válida
+
+            // Confirmar
+            _crearPO.PulsarCrearCompra();
+            _crearPO.ConfirmarModal();
+
+            // 3. ASSERT
+            // Esperamos redirección a Detalle
+            System.Threading.Thread.Sleep(2000); // DEJAMOS UN POCO DE TIEMPO PARA PROCESAR
+            bool urlCorrecta = _driver.Url.Contains("/Compra/DetailParaCompra");
+            Assert.True(urlCorrecta, $"Fallo: No se redirigió al detalle. URL actual: {_driver.Url}");
         }
     }
 }
