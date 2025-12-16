4262dea70010895d42f716ad49a2aaa9b1889889
DimasUCLM <dimasantonio.torralba@alu.uclm.es>
2025-12-16 16:47:20 +0100

    separada prueba compleja en 2

--- test/AppForSEII2526.UIT/CU-Compra/CUCrearCompra_UIT.cs
+++ test/AppForSEII2526.UIT/CU-Compra/CUCrearCompra_UIT.cs
@@ -284,11 +284,11 @@ namespace AppForSEII2526.UIT.CU_Compra
         }
 
         // -------------------------------------------------------------------
-        // [Fact]: PRUEBA DE CAMINO LARGO Y DANDO MUCHAS VUELTAS POR LA UI (Creación Exitosa)
+        // [Fact]: PRUEBA DE CANCELAR EL DIÁLOGO DE CONFIRMACIÓN (Creación Exitosa)
         // -------------------------------------------------------------------
         [Fact]
         [Trait("Category", "UIT")]
-        public void UC1_7_NavegacionModificarCarrito()
+        public void UC1_7_NavegacionCancelarDialogoCompra_Exito()
         {
             // 1. ARRANGE
             string nombre = "Yoel";
@@ -318,13 +318,54 @@ namespace AppForSEII2526.UIT.CU_Compra
             _crearPO.PulsarCrearCompra();
             // Retroceder
             _crearPO.RechazarModal();
-            //Me paseo por la pestaña de selección otra vez
-            _crearPO.PulsarModificarCarrito();
-            //Meto otro martillo porque venga
+            // Confirmar, ahora sí
+            _crearPO.PulsarCrearCompra();
+            _crearPO.ConfirmarModal();
+
+            // 3. ASSERT
+            // Esperamos redirección a Detalle
+            System.Threading.Thread.Sleep(2000); // DEJAMOS UN POCO DE TIEMPO PARA PROCESAR
+            bool urlCorrecta = _driver.Url.Contains("/Compra/DetailParaCompra");
+            Assert.True(urlCorrecta, $"Fallo: No se redirigió al detalle. URL actual: {_driver.Url}");
+        }
+
+        // -------------------------------------------------------------------
+        // [Fact]: PRUEBA DE REGRESAR AL CARRITO (Creación Exitosa)
+        // -------------------------------------------------------------------
+        [Fact]
+        [Trait("Category", "UIT")]
+        public void UC1_8_NavegacionModificarCarrito_Exito()
+        {
+            // 1. ARRANGE
+            string nombre = "Yoel";
+            string apellidos = "CS";
+            string direccionEnvio = "casa de yoel";
+            string pagoValue = "0";
+
+            int herramientaId = 1;
+            string nombreHerramienta = "Martillo";
+            string material = "Madera";
+            string precio = "10";
+
+            // 2. ACT
+            // Navegar
+            _driver.Navigate().GoToUrl(_URI + "Compra/SelectHerramientasParaCompra");
+
+            // Buscar y Seleccionar
             _selectPO.BuscarHerramientas(material, precio);
             _selectPO.AñadirHerramientasAlCarroDeCompra(nombreHerramienta);
             _selectPO.Continuar();
-            // Confirmar, ahora sí
+
+            // Rellenar Formulario
+            _crearPO.RellenarDatosGenerales(nombre, apellidos, direccionEnvio, pagoValue);
+            _crearPO.RellenarDescripcion(herramientaId, "Descripción cualquiera"); // cualquier descripción no nula es válida
+
+            // Retrocedo al carrito
+            _crearPO.PulsarModificarCarrito();
+            // Continuar
+            _selectPO.Continuar();
+
+            // Confirmar
             _crearPO.PulsarCrearCompra();
             _crearPO.ConfirmarModal();
 
